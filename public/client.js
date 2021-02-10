89792cb75563a6b369950d39438007679d71b90b
BadBonus <arturyes1993@gmail.com>
2021-02-10 13:26:55 +0300

    initial commit

--- public/client.js
+++ public/client.js
@@ -9,7 +9,6 @@ import { AfterimagePass } from "./jsm/postprocessing/AfterimagePass.js";
 import { OrbitControls } from "./jsm/controls/OrbitControls.js";
 
 let camera, scene, renderer, composer;
-let mesh, smartphone, duck;
 let edge = 0;
 let edgeDirection = false;
 
@@ -19,8 +18,14 @@ const params = {
   enable: true,
 };
 
+let uniforms = {
+  u_time: { type: "f", value: 1.0 },
+  u_resolution: { type: "v2", value: new THREE.Vector2() },
+  u_mouse: { type: "v2", value: new THREE.Vector2() },
+};
+
 init();
-createGUI();
+// createGUI();
 animate();
 
 function init() {
@@ -38,7 +43,7 @@ function init() {
   camera.position.z = 400;
 
   scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xbab595);
+  scene.background = new THREE.Color(0x000000);
   //   scene.fog = new THREE.Fog(0x000000, 1, 1000);
 
   scene.add(new THREE.AmbientLight(0x555555));
@@ -47,68 +52,38 @@ function init() {
   light.position.set(0, 500, 2000);
   scene.add(light);
 
-  const loader = new GLTFLoader();
-
-  let promiseModels1 = new Promise((resolve, reject) => {
-    loader.load("./assets/silver_smartphone/scene.gltf", function (gltf) {
-      duck = gltf.scene;
-      duck.name = "duck";
-      duck.position.set(-5, -30, 300);
-      duck.scale.set(100, 100, 100);
-      duck.visible = false;
-
-      // scene.add(duck);
-      // render();
-      resolve();
-    });
-  });
-
-  let promiseModels2 = new Promise((resolve, reject) => {
-    loader.load("./assets/smartphone/scene.gltf", function (gltf) {
-      smartphone = gltf.scene;
-      smartphone.position.set(0, -10, 300);
-      smartphone.scale.set(40, 40, 40);
-
-      gltf.scene.traverse(function (child) {
-        if (child instanceof THREE.Mesh) {
-          console.log(child);
-          child.geometry.needsUpdate = true;
-          for (
-            let index = 0;
-            index < child.geometry.attributes.position.count ;
-            index++
-          ) {
-            child.geometry.attributes.position[index] = 1.0;
-          }
-        }
-      });
-
-      // scene.add(smartphone);
-
-      // render();
-      resolve();
-    });
-  });
-
-  Promise.all([promiseModels1, promiseModels2]).then(() => {
-    let x = new THREE.Vector3();
-    scene.add(duck);
-    scene.add(smartphone);
-    smartphone.getWorldPosition(x);
-    render();
-    let y = scene.getObjectByName("duck");
-  });
-
-  // postprocessing
-
-  composer = new EffectComposer(renderer);
-  composer.addPass(new RenderPass(scene, camera));
-
-  afterimagePass = new AfterimagePass();
-  composer.addPass(afterimagePass);
-  afterimagePass.uniforms["damp"].value = 0.5;
-
   window.addEventListener("resize", onWindowResize);
+  document.onmousemove = function (e) {
+    uniforms.u_mouse.value.x = e.pageX;
+    uniforms.u_mouse.value.y = e.pageY;
+  };
+
+  const vertices = [];
+
+  for (let i = 0; i < 100000; i++) {
+    const x = THREE.MathUtils.randFloatSpread(2000);
+    const y = THREE.MathUtils.randFloatSpread(2000);
+    const z = THREE.MathUtils.randFloatSpread(2000);
+
+    vertices.push(x, y, z);
+  }
+
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute(
+    "position",
+    new THREE.Float32BufferAttribute(vertices, 3)
+  );
+
+  const material = new THREE.PointsMaterial({ color: 0xffffff });
+  // const shaderMaterial = new THREE.ShaderMaterial({
+  //   uniforms: uniforms,
+  //   vertexShader: document.getElementById("vertexShader").textContent,
+  //   fragmentShader: document.getElementById("fragmentShader").textContent,
+  // });
+
+  const points = new THREE.Points(geometry, material);
+
+  scene.add(points);
 
   const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -120,58 +95,23 @@ function init() {
 }
 
 function createGUI() {
-  const gui = new GUI({ name: "Damp setting" });
-  gui.add(afterimagePass.uniforms["damp"], "value", 0, 1).step(0.001);
-  gui.add(params, "enable");
+  // const gui = new GUI({ name: "Damp setting" });
+  // gui.add(afterimagePass.uniforms["damp"], "value", 0, 1).step(0.001);
+  // gui.add(params, "enable");
 }
 
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
+  uniforms.u_resolution.value.x = renderer.domElement.width;
+  uniforms.u_resolution.value.y = renderer.domElement.height;
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-  composer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function render() {
-  // console.log(edge);
-  // if (smartphone) {
-  //   // smartphone.rotation.x += 0.5;
-  //   if (edgeDirection) {
-  //     smartphone.rotation.y += 0.01 + edge / 100;
-  //     duck.rotation.y += 0.01 + edge / 100;
-  //     edge += 0.22;
-  //   } else {
-  //     smartphone.rotation.y += 0.01 - edge / 100;
-  //     duck.rotation.y += 0.01 - edge / 100;
-  //     edge -= 0.22;
-  //   }
-
-  //   if (edge >= 70) {
-  //     edgeDirection = false;
-  //     if (smartphone.visible && !duck.visible) {
-  //       smartphone.visible = !smartphone.visible;
-  //       duck.visible = !duck.visible;
-  //     } else {
-  //       smartphone.visible = !smartphone.visible;
-  //       duck.visible = !duck.visible;
-  //     }
-  //   } else if (edge <= 0) {
-  //     edgeDirection = true;
-  //   }
-  // }
-
-  // if (params.enable) {
-  //   composer.render();
-  // } else {
-  //   renderer.render(scene, camera);
-  // }
-
-  if (edge > 30) {
-    composer.render();
-  } else if (edge < 30) {
-    renderer.render(scene, camera);
-  }
+  uniforms.u_time.value += 0.05;
+  renderer.render(scene, camera);
 }
 
 function animate() {

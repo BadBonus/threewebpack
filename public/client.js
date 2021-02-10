81e1299cad3f66e4add6fe34f3ea0e70b1d94d04
BadBonus <arturyes1993@gmail.com>
2021-02-10 23:53:59 +0300

    added 2 particled meshes

--- public/client.js
+++ public/client.js
@@ -40,7 +40,7 @@ function init() {
     0.1,
     1000
   );
-  camera.position.z = 400;
+  camera.position.z = 10;
 
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0x000000);
@@ -71,19 +71,67 @@ function init() {
   const geometry = new THREE.BufferGeometry();
   geometry.setAttribute(
     "position",
-    new THREE.Float32BufferAttribute(vertices, 3)
+    new THREE.Float32BufferAttribute(
+      [
+        0.0,
+        0.0,
+        0.0,
+        10.0,
+        10.0,
+        10.0,
+        -10.0,
+        -10.0,
+        -10.0,
+
+        20.0,
+        20.0,
+        20.0,
+        -20.0,
+        10.0,
+        10.0,
+        -10.0,
+        -20.0,
+        10.0,
+      ],
+      3
+    )
   );
 
-  const material = new THREE.PointsMaterial({ color: 0xffffff });
-  // const shaderMaterial = new THREE.ShaderMaterial({
-  //   uniforms: uniforms,
-  //   vertexShader: document.getElementById("vertexShader").textContent,
-  //   fragmentShader: document.getElementById("fragmentShader").textContent,
-  // });
+  const material = new THREE.PointsMaterial({
+    color: 0xffff00,
+    size: 0.2,
+    transparent: true,
+  });
+  const shaderMaterial = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader: document.getElementById("vertexShader").textContent,
+    fragmentShader: document.getElementById("fragmentShader").textContent,
+  });
 
-  const points = new THREE.Points(geometry, material);
+  // const points = new THREE.Points(geometry, material);
 
-  scene.add(points);
+  // scene.add(points);
+
+  let boxGeometry = new THREE.BoxGeometry(5, 5, 5, 11, 11, 11);
+  let cylinderGeometry = new THREE.CylinderGeometry(2, 2, 10, 30, 30);
+
+  let box = createPointsCloud(boxGeometry, material);
+  let cylinder = createPointsCloud(cylinderGeometry, shaderMaterial);
+  cylinder.position.x = 5;
+  box.position.x = -5;
+
+  const numVertices = cylinderGeometry.attributes.position.count;
+  var indexes = new Float32Array(numVertices * 1); // 1 values per vertex
+
+  for (var i = 1; i <= numVertices; i++) {
+    // set alpha randomly
+    indexes[i] = numVertices - [i];
+  }
+
+  cylinderGeometry.addAttribute( 'a_index', new THREE.BufferAttribute( indexes, 1 ) );
+
+  scene.add(box);
+  scene.add(cylinder);
 
   const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -117,4 +165,18 @@ function render() {
 function animate() {
   requestAnimationFrame(animate);
   render();
+}
+
+/* Create point cloud */
+function createPointsCloud(geometry, material) {
+  /* Sprite material */
+  let spriteMaterial = new THREE.PointsMaterial({
+    color: 0xffffff,
+    size: 0.23,
+    transparent: true,
+  });
+
+  let points = new THREE.Points(geometry, material ? material : spriteMaterial);
+
+  return points;
 }
